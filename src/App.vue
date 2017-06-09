1d11d84939ce49595974b256f5914fd5578b3c6a
muzl3 <muzl3@lenovo.com>
2017-06-09 17:41:38 +0800

    [mzl] #add table to index.vue

--- src/App.vue
+++ src/App.vue
@@ -11,19 +11,18 @@
     <div class="main-body">
       <div class="container">
         <el-row>
-            <el-col :span="8">
-              <el-menu default-active="2" router @open="handleOpen" @close="handleClose">
+            <el-col :span="4">
+              <el-menu default-active="/" router @open="handleOpen" @close="handleClose">
                 <el-submenu index="1">
                   <template slot="title"><i class="el-icon-message"></i>导航一</template>
                   <el-menu-item index="/">Index</el-menu-item>
                   <el-menu-item index="Content">Content</el-menu-item>
-                  <el-menu-item index="1-3">选项3</el-menu-item>
                 </el-submenu>
                 <el-menu-item index="2"><i class="el-icon-menu"></i>导航二</el-menu-item>
                 <el-menu-item index="3"><i class="el-icon-setting"></i>导航三</el-menu-item>
               </el-menu>
             </el-col>
-            <el-col :span="16">
+            <el-col :span="20">
               <router-view></router-view>
             </el-col>
         </el-row>
